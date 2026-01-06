4d6eae8b86a1906a9e786a44f3846b2b7f4712e5
mukuru1 <kabengaedwin@gmail.com>
2026-01-06 16:35:48 +0200

    commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import Hero from "./components/hero";
+import HowItWorks from "./components/cards";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <HowItWorks />
+    </>
+  );
+}
